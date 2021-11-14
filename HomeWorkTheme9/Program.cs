d0b8365ea5edce31de9852aeffca450fbe352151
Богдан <b.kornitskii@g.nsu.ru>
2021-11-14 18:08:07 +0700

    добавлено отправка и получение изображений и аудиофайлов(отправка пока не работает)

--- HomeWorkTheme9/Program.cs
+++ HomeWorkTheme9/Program.cs
@@ -15,14 +15,16 @@ namespace HomeWorkTheme9
     class Program
     {
         static TelegramBotClient bot;
+        static string nameCommand;
         static void Main(string[] args)
         {
-            //Directory.CreateDirectory("test");
             //вставьте свой токен
             string token = File.ReadAllText($"{ Directory.GetCurrentDirectory()}//token.txt ");
             bot = new TelegramBotClient(token);
             bot.StartReceiving();
 
+            bot.OnMessage += MessageCommand;
+ 
             bot.OnMessage += MessageListener;
 
             Console.ReadLine();
@@ -30,13 +32,52 @@ namespace HomeWorkTheme9
             bot.StopReceiving();
 
         }
-        private static async void GetMe()
-        {
-            var me = await bot.GetMeAsync();
-            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}");
-        }
 
         private static async void MessageListener(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(nameCommand);
+            var msg = e.Message;
+            switch (nameCommand)
+            {
+                case "Отправить документ":
+                    DownLoad(e.Message.Document.FileId, $"{Directory.GetCurrentDirectory()}//file//{e.Message.Document.FileName}");
+                    break;
+
+                case "Получить документ":
+                    await bot.SendDocumentAsync(
+                        msg.Chat.Id,
+                        System.IO.File.Open($"{Directory.GetCurrentDirectory()}\\{msg.Text}", FileMode.Open)
+                        );
+                    break;
+                case "Отправить аудио":
+                    DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                    //DownLoad(e.Message.Document.FileId, $"{Directory.GetCurrentDirectory()}//audio//{e.Message.Document.FileName}");
+                    break;
+
+                case "Получить аудио":
+                    await bot.SendAudioAsync(
+                        msg.Chat.Id,
+                        System.IO.File.Open($"{Directory.GetCurrentDirectory()}\\{msg.Text}", FileMode.Open)
+                        );
+                    break;
+                case "Отправить изображение":
+                    DownLoad(e.Message.Document.FileId, $"{Directory.GetCurrentDirectory()}//pictures//{e.Message.Document.FileName}");
+                    break;
+
+                case "Получить изображение":
+                    await bot.SendPhotoAsync(
+                        msg.Chat.Id,
+                        File.Open($"{Directory.GetCurrentDirectory()}\\{msg.Text}", FileMode.Open)
+                        );
+                    break;
+                default:
+                    break;
+            }
+            Thread.Sleep(300);
+        }
+
+
+        private static async void MessageCommand(object sender, MessageEventArgs e)
         {
             
             var msg = e.Message;
@@ -48,21 +89,44 @@ namespace HomeWorkTheme9
                         await bot.SendTextMessageAsync(
                             msg.Chat.Id,
                             "Выберите файл, который вы хотите отправить");
-                        DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                        nameCommand = "Отправить документ";
                         break;
+
                     case "Получить документ":
                         await bot.SendTextMessageAsync(
                             msg.Chat.Id,
                             "Выберите документ, который вы хотите получить, напишите название файла в чат");
-                        
-                        GetNameFile(msg.Chat.Id);
-                        Thread.Sleep(3000);
-                        await bot.SendDocumentAsync(
+                        GetNameFile(msg.Chat.Id, "file");
+                        nameCommand = "Получить документ";
+                        break;
+                    case "Отправить аудио":
+                        await bot.SendTextMessageAsync(
                             msg.Chat.Id,
-                            System.IO.File.Open(msg.Text, FileMode.Open)
-                            );
+                            "Выберите аудио, которое вы хотите отправить");
+                        nameCommand = "Отправить аудио";
                         break;
 
+                    case "Получить аудио":
+                        await bot.SendTextMessageAsync(
+                            msg.Chat.Id,
+                            "Выберите аудио, которое вы хотите получить, напишите название аудио в чат");
+                        GetNameFile(msg.Chat.Id, "audio");
+                        nameCommand = "Получить аудио";
+                        break;
+                    case "Отправить изображение":
+                        await bot.SendTextMessageAsync(
+                            msg.Chat.Id,
+                            "Выберите изображение, которое вы хотите отправить");
+                        nameCommand = "Отправить изображение";
+                        break;
+
+                    case "Получить изображение":
+                        await bot.SendTextMessageAsync(
+                            msg.Chat.Id,
+                            "Выберите изображение, которое вы хотите получить, напишите название изображение в чат");
+                        GetNameFile(msg.Chat.Id, "pictures");
+                        nameCommand = "Получить изображение";
+                        break;
                     default:
                         await bot.SendTextMessageAsync
                         (
@@ -82,9 +146,9 @@ namespace HomeWorkTheme9
         /// отправить названия файлов, хранящиеся в директории
         /// </summary>
         /// <param name="ChatId"></param>
-        private static async void GetNameFile(long ChatId)
+        private static async void GetNameFile(long ChatId, string nameDirectory)
         {
-            var file = Directory.GetFiles("test");
+            var file = Directory.GetFiles($"{nameDirectory}//");
             foreach (var elem in file)
             {
                 await bot.SendTextMessageAsync(
@@ -104,7 +168,7 @@ namespace HomeWorkTheme9
                 {
                     new List<KeyboardButton> { new KeyboardButton { Text = "Отправить документ" }, new KeyboardButton { Text = "Получить документ" } },
                     new List<KeyboardButton> { new KeyboardButton { Text = "Отправить аудио" }, new KeyboardButton { Text = "Получить аудио" } },
-                    new List<KeyboardButton> { new KeyboardButton { Text = "Отправить картинку" }, new KeyboardButton { Text = "Получить картинку" } }
+                    new List<KeyboardButton> { new KeyboardButton { Text = "Отправить изображение" }, new KeyboardButton { Text = "Получить изображение" } }
                 }
             };
         }
@@ -116,7 +180,7 @@ namespace HomeWorkTheme9
         static async void DownLoad(string fileId, string path)
         {
             var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
+            FileStream fs = new FileStream(path, FileMode.Create);
             await bot.DownloadFileAsync(file.FilePath, fs);
             fs.Close();
 
@@ -124,16 +188,3 @@ namespace HomeWorkTheme9
         }
     }
 }
-// Создать бота, позволяющего принимать разные типы файлов, 
-// *Научить бота отправлять выбранный файл в ответ
-// 
-// https://data.mos.ru/
-// https://apidata.mos.ru/
-// 
-// https://vk.com/dev
-// https://vk.com/dev/manuals
-
-// https://dev.twitch.tv/
-// https://discordapp.com/developers/docs/intro
-// https://discordapp.com/developers/applications/
-// https://discordapp.com/verification
